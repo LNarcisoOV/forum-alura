7ae5129313b840c28abb81731846484479ad7164
Leonardo Narciso <LNarcisoOV@gmail.com>
2020-10-05 16:18:49 -0300

    Enabling authentication by the user.

--- src/main/java/br/com/alura/forum/configuracao/SpringSecurityConfig.java
+++ src/main/java/br/com/alura/forum/configuracao/SpringSecurityConfig.java
@@ -1,23 +1,36 @@
 package br.com.alura.forum.configuracao;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import br.com.alura.forum.service.AutenticacaoService;
 
 @Configuration
 @EnableWebSecurity
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter{
 
+	@Autowired
+	private AutenticacaoService autenticacaoService;
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(autenticacaoService).passwordEncoder(new BCryptPasswordEncoder());
+	}
 	
 	//Authorization config
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-		.antMatchers(HttpMethod.GET, "/topicos").permitAll()
-		.antMatchers(HttpMethod.GET, "/topicos/*").permitAll();
+			.antMatchers(HttpMethod.GET, "/topicos").permitAll()
+			.antMatchers(HttpMethod.GET, "/topicos/*").permitAll()
+			.anyRequest().authenticated()
+		.and()
+			.formLogin();
 	}
 }
