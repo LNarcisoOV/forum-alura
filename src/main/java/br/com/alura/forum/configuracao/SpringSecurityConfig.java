10f67d05b74a92ce842316876fd073012c1bb5dd
Leonardo Narciso <LNarcisoOV@gmail.com>
2020-10-05 11:35:50 -0300

    Adding permissions to some URLs.

--- src/main/java/br/com/alura/forum/configuracao/SpringSecurityConfig.java
+++ src/main/java/br/com/alura/forum/configuracao/SpringSecurityConfig.java
@@ -1,6 +1,10 @@
 package br.com.alura.forum.configuracao;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -9,4 +13,11 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter{
 
 	
+	//Authorization config
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+		.antMatchers(HttpMethod.GET, "/topicos").permitAll()
+		.antMatchers(HttpMethod.GET, "/topicos/*").permitAll();
+	}
 }
